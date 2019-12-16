64262789e611f75f2d569cdb9be92e791ee904e1
jokojr97 <joko.riyadi97@gmail.com>
2019-12-16 20:51:38 +0700

    update

--- app/src/main/java/com/example/sqliteapp_java/MainActivity.java
+++ app/src/main/java/com/example/sqliteapp_java/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.sqliteapp_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
+
+    static MyDB myDB;
+    ArrayList<String>arrayListNama;
+    ArrayList<Integer>arrayListNRP;
+    ListView listView;
+    Button buttonTambah;
+    ArrayList<String>arrayListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void fungsiTambahData(View view) {
     }
 }
--- app/src/main/java/com/example/sqliteapp_java/MyDB.java
+++ app/src/main/java/com/example/sqliteapp_java/MyDB.java
@@ -1,12 +1,13 @@
 package com.example.sqliteapp_java;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 import android.widget.Toast;
 
-public class MyDB implements SQLiteOpenHelper {
+public class MyDB extends SQLiteOpenHelper {
     Context mContext;
     public static String DATABASE_NAME = "mahasiswa";
     public static int VERSION = 1;
@@ -46,4 +47,53 @@ public class MyDB implements SQLiteOpenHelper {
             return false;
         }
     }
+
+    //menampilkan record pada tabel mahasiswa by nrp
+    public Cursor tampilDataMahasiswa(String nrp){
+        try {
+            String sql = "select * from biodata where nrp="+nrp+";";
+            SQLiteDatabase db = getReadableDatabase();
+            Cursor cursor = db.rawQuery(sql, null);
+            return cursor;
+        }catch (Exception e){
+            return null;
+        }
+    }
+
+    //menampilkan record pada tabel mahasiswa
+    public Cursor tampilSemuaDataMahasiswa(String nrp){
+        try {
+            String sql = "select * from biodata;";
+            SQLiteDatabase db = getReadableDatabase();
+            Cursor cursor = db.rawQuery(sql, null);
+            return cursor;
+        }catch (Exception e){
+            return null;
+        }
+    }
+
+    //Mengupdate record pada tabel mahasiswa
+    public boolean updateData(int nrpLama, int nrpBaru, String nama, String alamat){
+        try {
+            String sql = "update biodata set" +
+                    " nrp="+nrpBaru+", nama='"+nama+"', alamat='"+alamat+"' where nrp="+nrpLama+";";
+            SQLiteDatabase db = getWritableDatabase();
+            db.execSQL(sql);
+            return  true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public boolean delete(int nrpLama){
+        try {
+            String sql = "delete from biodata where nrp="+nrpLama+";";
+            SQLiteDatabase db = getWritableDatabase();
+            db.execSQL(sql);
+            return  true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
 }
